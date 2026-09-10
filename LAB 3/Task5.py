8c8e77a158489a131b1223f1cd53303ed038eddd
Dania Khan <142868029+daniakhan123@users.noreply.github.com>
2026-09-10 15:10:06 +0500

    Update Task5.py

--- LAB 3/Task5.py
+++ LAB 3/Task5.py
@@ -1,1 +1,24 @@
+import numpy as np
 
+theta = np.radians(30)
+tx, ty = 5, 3
+
+R = np.array([
+    [np.cos(theta), -np.sin(theta), 0],
+    [np.sin(theta), np.cos(theta), 0],
+    [0, 0, 1]
+])
+
+T = np.array([
+    [1, 0, tx],
+    [0, 1, ty],
+    [0, 0, 1]
+])
+
+Rigid = T @ R
+
+point = np.array([2, 1, 1])
+new_point = Rigid @ point
+
+print(Rigid)
+print(new_point)
